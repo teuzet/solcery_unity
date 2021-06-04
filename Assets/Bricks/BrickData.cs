4dc9cdc23df77d7bbcd2dc2ea05f87198dc1961d
teuzet <42753376+teuzet@users.noreply.github.com>
2021-06-04 17:00:29 +0300

    simple manual serialization for BrickData

--- Assets/Bricks/BrickData.cs
+++ Assets/Bricks/BrickData.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using System.Collections.Generic;
 
 namespace Grimmz
@@ -18,15 +19,24 @@ namespace Grimmz
 
         public BrickData[] Slots;
 
-
         public BrickData(BrickConfig config)
         {
             Type = (int)config.Type;
-            ///
-
             UnityEngine.Debug.Log($"Setting Slots to {config.Slots.Count}");
             Slots = new BrickData[config.Slots.Count];
             UnityEngine.Debug.Log(Slots.Length);
         }
+
+        public void SerializeToBytes(ref List<byte> buffer) {
+            buffer.AddRange(BitConverter.GetBytes(Type).ToList<byte>());
+            buffer.AddRange(BitConverter.GetBytes(Subtype).ToList<byte>());
+            if (HasObjectSelection)
+                buffer.AddRange(BitConverter.GetBytes(Object).ToList<byte>());
+            if (IntField >= 0)
+                buffer.AddRange(BitConverter.GetBytes(IntField).ToList<byte>());
+            foreach (BrickData child in Slots) 
+                child.SerializeToBytes(ref buffer);
+        }
     }
+
 }
