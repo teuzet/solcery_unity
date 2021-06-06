49b22ccc498e3f00a4115bc1d18350bdbd09cb35
teuzet <42753376+teuzet@users.noreply.github.com>
2021-06-06 19:27:15 +0300

    CardMetadata deserialization

--- Assets/CardMetadata.cs
+++ Assets/CardMetadata.cs
@@ -5,17 +5,32 @@ using System.Text;
 
 namespace Grimmz
 {
+
     public struct CardMetadata
     {
-        int Picture;
-        string Name;
-        string Description;
+        public int Picture;
+        public string Name;
+        public string Description;
 
         public CardMetadata(bool ignored)
         {
             Picture = 1;
             Name = "Card";
             Description = "Description";
+        }
+
+        public CardMetadata(string joinedBuffer) {
+            var splitted = joinedBuffer.Split('|');
+            List<byte> bytesList = new List<byte>();
+            foreach (string s in splitted) {
+                bytesList.Add((byte)int.Parse(s));
+            }
+            var bytes = bytesList.ToArray();
+            Picture = BitConverter.ToInt32(bytes, 0);
+            int nameLength = BitConverter.ToInt32(bytes, 4);
+            Name = System.Text.Encoding.UTF8.GetString(bytes, 8, nameLength);
+            int descriptionLength = BitConverter.ToInt32(bytes, 8 + nameLength);
+            Description = System.Text.Encoding.UTF8.GetString(bytes, nameLength + 12 , descriptionLength);
         }
 
         public void SerializeToBytes(ref List<byte> buffer)
@@ -30,5 +45,7 @@ namespace Grimmz
             tmpBuffer.AddRange(Encoding.UTF8.GetBytes(Description).ToList<byte>());
             buffer.AddRange(tmpBuffer);
         }
+
+
     }
 }
